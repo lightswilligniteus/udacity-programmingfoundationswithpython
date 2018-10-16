e73b87fd4c7a5f91e25376f42a860cb7acc3dd60
kdaya <kdayathegamer@gmail.com>
2018-10-16 15:01:53 +1100

    add Triangle and remove repetative code

--- LessonThree-UseClassesDrawTurtles/mindstorms.py
+++ LessonThree-UseClassesDrawTurtles/mindstorms.py
@@ -7,20 +7,27 @@ def draw_square():
 
     brad = turtle.Turtle()
     brad.color("yellow")
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+    for x in range (4):
+        brad.forward(100)
+        brad.right(90)
 
+def draw_circle():
     angie = turtle.Turtle()
-    angie.shape("arrow")
     angie.color("blue")
     angie.circle(100)
 
+
+def draw_triangle():
+    abby = turtle.Turtle()
+    abby.color("white")
+    for x in range (3):
+        abby.forward(100)
+        abby.left(120)
+
     window.exitonclick()
 
+
+
 draw_square()
+draw_circle()
+draw_triangle()
