9bd4e65e10e5b2837bdeb142f20706c706eeb9b7
kdaya <kdayathegamer@gmail.com>
2018-10-16 14:50:29 +1100

    turtle exercise step 10

---
+++ LessonThree-UseClassesDrawTurtles/mindstorms.py
@@ -0,0 +1,26 @@
+#"actual thing that moves around to draw things"
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.color("yellow")
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
+
+    window.exitonclick()
+
+draw_square()
