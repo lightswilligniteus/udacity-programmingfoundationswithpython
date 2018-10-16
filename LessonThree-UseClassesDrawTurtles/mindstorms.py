30e55af1bc284026e46c06b15be563080d9725fe
kdaya <kdayathegamer@gmail.com>
2018-10-16 15:04:27 +1100

    add comments

--- LessonThree-UseClassesDrawTurtles/mindstorms.py
+++ LessonThree-UseClassesDrawTurtles/mindstorms.py
@@ -1,6 +1,7 @@
 #"actual thing that moves around to draw things"
 import turtle
 
+#code to draw a square
 def draw_square():
     window = turtle.Screen()
     window.bgcolor("red")
@@ -11,12 +12,13 @@ def draw_square():
         brad.forward(100)
         brad.right(90)
 
+#code to draw a circle
 def draw_circle():
     angie = turtle.Turtle()
     angie.color("blue")
     angie.circle(100)
 
-
+#code to draw a triangle
 def draw_triangle():
     abby = turtle.Turtle()
     abby.color("white")
@@ -27,7 +29,7 @@ def draw_triangle():
     window.exitonclick()
 
 
-
+#calling the three functions
 draw_square()
 draw_circle()
 draw_triangle()
