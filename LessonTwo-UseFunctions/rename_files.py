7283f55eb56f747b754e0177ec2cf87c8997a177
kdaya <kdayathegamer@gmail.com>
2018-10-16 14:23:50 +1100

    rename function completed

--- LessonTwo-UseFunctions/rename_files.py
+++ LessonTwo-UseFunctions/rename_files.py
@@ -2,7 +2,14 @@ import os
 
 def rename_files():
   #(1) get file names from a folder
-
+  #() is location path for files
+  file_list = os.listdir()
+  saved_path = os.getcwd()
+  print("Current Working Directory is " + saved_path)
+  os.chdir()
   #(2) for each file, rename file
+  for file_name in file_list:
+      os.rename(file_name, file_name.translate(None, "0123456789"))
+  os.chdir(saved_path)
 
 rename_files()
