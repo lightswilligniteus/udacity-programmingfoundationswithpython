0624ea5be13fc3d719f33e4827e4b84a3ed3e712
kdaya <kdayathegamer@gmail.com>
2018-10-16 14:17:38 +1100

    update

--- LessonTwo-UseFunctions/break_time.py
+++ LessonTwo-UseFunctions/break_time.py
@@ -1,3 +1,12 @@
 import webbrowser
+import time
 
-webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+#counter for maximum calls for the while loop and counter
+count = 0
+maxcount = 3
+
+#while loop that is called up to maxcount and opens a web browser every 2 hours.
+while count < maxcount:
+    time.sleep(7200)
+    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    count = count + 1
