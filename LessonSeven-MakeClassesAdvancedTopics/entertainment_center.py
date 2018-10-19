f9627d17fc9810de8382a36c58aebcadbfb19769
kdaya <kdayathegamer@gmail.com>
2018-10-19 13:11:23 +1100

    more inheritance

--- LessonSeven-MakeClassesAdvancedTopics/entertainment_center.py
+++ LessonSeven-MakeClassesAdvancedTopics/entertainment_center.py
@@ -4,25 +4,28 @@ import fresh_tomatoes
 toy_story = media.Movie("Toy Story",
             "A story of a boy and his toys that come to life",
             "http://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg",
-            "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+            "https://www.youtube.com/watch?v=KYz2wyBy3kc",
+            "10 minutes")
 
 #print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
                     "A marine on an alien planet",
                     "https://en.wikipedia.org/wiki/Avatar_(2009_film)#/media/File:Avatar-Teaser-Poster.jpg",
-                    "https://www.youtube.com/watch?v=6ziBFh3V1aM")
+                    "https://www.youtube.com/watch?v=6ziBFh3V1aM",
+                    "10 minutes")
 
 #print(avatar.storyline)
 
 pokemon = media.Movie("Pokemon: The First Movie",
                     "A story about pocket monsters",
                     "https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Pokemon-mewtwo-strikes-back.jpg/220px-Pokemon-mewtwo-strikes-back.jpg",
-                    "https://www.youtube.com/watch?v=ZybYS201lUw")
+                    "https://www.youtube.com/watch?v=ZybYS201lUw",
+                    "15 minutes")
 
+print(pokemon.duration)
 #pokemon.show_trailer()
 #movies = [toy_story, avatar, pokemon]
 #fresh_tomatoes.open_movies_page(movies)
 
 #print(media.Movie.VALID_RATINGS)
-print(media.Movie.__doc__)
--- LessonSeven-MakeClassesAdvancedTopics/media.py
+++ LessonSeven-MakeClassesAdvancedTopics/media.py
@@ -1,13 +1,18 @@
 import webbrowser
 
-class Movie():
+class Video():
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+
+class Movie(Video):
     #defining the __doc__ below in """
     """ This class provides a way to store movie related information"""
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, duration):
+        Video.__init__(self, movie_title, duration)
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
